594407c0f4c5ddd85020ccc9b7f725bdc9716314
Keyla R. Oliveira <keylarocha@hotmail.com.br>
2022-08-02 21:49:03 -0300

    Database selects added

---
+++ selects_BD_loja.sql
@@ -0,0 +1,24 @@
+## Customers
+select count(*) from customers;
+
+select * from customers where creditLimit >= 80000;
+
+select * from customers where state in ('NY', 'BC');
+
+## Offices
+select count(*) from offices;
+
+##employees
+select count(*) from employees;
+
+## Product lines
+select count(*) from productlines;
+
+##Order details
+
+## Orders
+select count(*) from orders;
+
+##Payments
+
+##Products
